4e4c1181d965575bf8146eaa06ebb000c5aa8515
Ilyesfellah <ilyesfellah.if@gmail.com>
2025-03-24 15:36:46 +0100

    Update accueil.php

--- php/accueil.php
+++ php/accueil.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -12,44 +8,25 @@ session_start();
   <link href="https://fonts.googleapis.com" rel="preconnect"/>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&amp;display=swap" rel="stylesheet"/>
 </head>
- 
+
 <body>
   <nav>
     <div class="logo">
       <img alt="My Trips Logo" src="logo_my_trips.png"/>
     </div>
     <ul>
-      <li>
-        <a class="active" href="accueil.php">Accueil</a>
-      </li>
-      <li>
-        <a href="présentation.php">Présentation</a>
-      </li>
-      <li>
-        <a href="rechercher.php">Rechercher</a>
-      </li>
-      <li>
-        <a href="mon_profil.php">Mon Profil</a>
-      </li>
+      <li><a class="active" href="accueil.php">Accueil</a></li>
+      <li><a href="présentation.php">Présentation</a></li>
+      <li><a href="rechercher.php">Rechercher</a></li>
+      <li><a href="mon_profil.php">Mon Profil</a></li>
       <?php if (isset($_SESSION['user'])): ?>
-        <li>
-          <a href="mon_profil.php">Mon Profil</a>
-        </li>
-        <li>
-          <a href="deconnexion.php">Se déconnecter</a>
-        </li>
+        <li><a href="mon_profil.php">Mon Profil</a></li>
+        <li><a href="deconnexion.php">Se déconnecter</a></li>
       <?php else: ?>
-        <li>
-          <a href="inscription.php">S'inscrire</a>
-        </li>
-        <li>
-          <a href="connexion.php">Se connecter</a>
-        </li>
+        <li><a href="inscription.php">S'inscrire</a></li>
+        <li><a href="connexion.php">Se connecter</a></li>
       <?php endif; ?>
-     
-      <li>
-        <a class="btn-primary" href="reserver.php">Réserver</a>
-      </li>
+      <li><a class="btn-primary" href="reserver.php">Réserver</a></li>
     </ul>
   </nav>
 
@@ -61,53 +38,32 @@ session_start();
     </div>
   </header>
 
-  <section class="destinations" id="destinations" style="display:flex;">
-    <div class="destination-card">
-      <img alt="Porto-Novo" src="https://www.les-covoyageurs.com/ressources/images-lieux/450-visiter-porto-novo-benin.jpg"/>
-      <h3>Porto-Novo</h3>
-    </div>
-    <div class="destination-card">
-      <img alt="Ouidah" src="https://geoconfluences.ens-lyon.fr/images/image-a-la-une/img-rieucau/doc-3-la-porte-du-non-retour.jpg"/>
-      <h3>Ouidah</h3>
-    </div>
-    <div class="destination-card">
-      <img alt="Abomey" src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Abomey-Königspalast2.jpg"/>
-      <h3>Abomey</h3>
-    </div>
-    <div class="destination-card">
-      <img alt="Ganvié" src="https://www.build-green.fr/wp-content/uploads/2023/06/village-flottant-ganvie-benin_03.jpg"/>
-      <h3>Ganvié</h3>
-    </div>
-    <div class="destination-card">
-      <img alt="Grand Popo" src="https://www.gouv.bj/upload/thumbnails/articles//0548016001606546234.png"/>
-      <h3>Grand Popo</h3>
-    </div>
-    <div class="destination-card">
-      <img alt="Parakou" src="https://upload.wikimedia.org/wikipedia/commons/2/21/La_colombe_18.jpg"/>
-      <h3>Parakou</h3>
-    </div>
-    <div class="destination-card">
-      <img alt="Natitingou" src="https://www.gouv.bj/upload/thumbnails/articles//0952356001605489551.jpeg"/>
-      <h3>Natitingou</h3>
-    </div>
-    <div class="destination-card">
-      <img alt="Dassa" src="https://expatstraveltogether.com/wp-content/uploads/2023/09/DIG16_view-hills-city-dassazoume-benin-450w-1301045479-scaled-1.jpg"/>
-      <h3>Dassa</h3>
-    </div>
-    <div class="destination-card">
-      <img alt="Tanguiéta" src="https://cenozo.org/wp-content/uploads/2021/03/Entree-du-Parc-National-de-la-Pendjari-a-Batia.jpg"/>
-      <h3>Tanguiéta</h3>
-    </div>
-    <div class="destination-card">
-      <img alt="Possotomé" src="https://www.ecobenin.org/wp-content/uploads/Possotome_cocotier_plage_chez_prefet_pilotis_lac_aheme_ecotourisme_ecobenin_benin.jpg"/>
-      <h3>Possotomé</h3>
-    </div>
+  <section class="destinations" id="destinations" style="display: flex; flex-wrap: wrap;">
+    <?php
+    $destinations = [
+        ['name' => 'Porto-Novo', 'img' => 'https://www.les-covoyageurs.com/ressources/images-lieux/450-visiter-porto-novo-benin.jpg'],
+        ['name' => 'Ouidah', 'img' => 'https://geoconfluences.ens-lyon.fr/images/image-a-la-une/img-rieucau/doc-3-la-porte-du-non-retour.jpg'],
+        ['name' => 'Abomey', 'img' => 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Abomey-Königspalast2.jpg'],
+        ['name' => 'Ganvié', 'img' => 'https://www.build-green.fr/wp-content/uploads/2023/06/village-flottant-ganvie-benin_03.jpg'],
+        ['name' => 'Grand Popo', 'img' => 'https://www.gouv.bj/upload/thumbnails/articles//0548016001606546234.png'],
+        ['name' => 'Parakou', 'img' => 'https://upload.wikimedia.org/wikipedia/commons/2/21/La_colombe_18.jpg'],
+        ['name' => 'Natitingou', 'img' => 'https://www.gouv.bj/upload/thumbnails/articles//0952356001605489551.jpeg'],
+        ['name' => 'Dassa', 'img' => 'https://expatstraveltogether.com/wp-content/uploads/2023/09/DIG16_view-hills-city-dassazoume-benin-450w-1301045479-scaled-1.jpg'],
+        ['name' => 'Tanguiéta', 'img' => 'https://cenozo.org/wp-content/uploads/2021/03/Entree-du-Parc-National-de-la-Pendjari-a-Batia.jpg'],
+        ['name' => 'Possotomé', 'img' => 'https://www.ecobenin.org/wp-content/uploads/Possotome_cocotier_plage_chez_prefet_pilotis_lac_aheme_ecotourisme_ecobenin_benin.jpg'],
+    ];
+
+    foreach ($destinations as $destination) {
+        echo '<div class="destination-card">';
+        echo '<img alt="' . $destination['name'] . '" src="' . $destination['img'] . '"/>';
+        echo '<h3>' . $destination['name'] . '</h3>';
+        echo '</div>';
+    }
+    ?>
   </section>
 
   <footer>
-    <p>
-      © 2025 My Trips. Tous droits réservés.
-    </p>
+    <p>© 2025 My Trips. Tous droits réservés.</p>
   </footer>
 </body>
 </html>
